a88aa9b12ee30d37446abe6c5be56360138f17ec
David Garza <dgarza0413@gmail.com>
2020-09-09 13:04:29 -0500

    added custom error handling

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -18,6 +18,8 @@ import Slide from "@material-ui/core/Slide";
 import Gavel from "@material-ui/icons/Gavel";
 import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 
+import Error from '../Shared/Error';
+
 function Transition(props) {
   return <Slide direction="up" {...props} />
 }
@@ -84,7 +86,7 @@ const Register = ({ classes, setNewUser }) => {
                 Previous User? Login Here
             </Button>
 
-              {error && <div>Error</div>}
+              {error && <div><Error error={error} /></div>}
             </form>
           )
         }}
