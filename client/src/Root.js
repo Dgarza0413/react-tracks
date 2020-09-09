1ed23428fd4bcf3cae5e41be287b68a3d7ad3513
David Garza <dgarza0413@gmail.com>
2020-09-09 15:49:54 -0500

    adding routing with react router 4

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,8 +1,13 @@
 import React from "react";
-import withRoot from "./withRoot";
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
+
+import withRoot from "./withRoot";
+import App from './pages/App';
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header';
 
 const Root = () => (
     <Query query={GET_TRACKS_QUERY}>
@@ -10,7 +15,17 @@ const Root = () => (
             if (loading) return <div>loading</div>
             if (error) return <div>Error</div>
 
-            return <div>{JSON.stringify(data)}</div>
+            return (
+                <Router>
+                    <>
+                        <Header />
+                        <Switch>
+                            <Route exact path="/" component={App} />
+                            <Route path="/profile/:id" component={Profile} />
+                        </Switch>
+                    </>
+                </Router>
+            )
         }}
     </Query>
 );
