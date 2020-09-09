c2b2ea0d0d5c4c35652a6c56e6c6bb3702704708
David Garza <dgarza0413@gmail.com>
2020-09-09 15:59:23 -0500

    styling the header

--- client/src/Root.js
+++ client/src/Root.js
@@ -10,15 +10,16 @@ import Profile from './pages/Profile';
 import Header from './components/Shared/Header';
 
 const Root = () => (
-    <Query query={GET_TRACKS_QUERY}>
+    <Query query={ME_QUERY}>
         {({ data, loading, error }) => {
             if (loading) return <div>loading</div>
             if (error) return <div>Error</div>
+            const currentUser = data.me
 
             return (
                 <Router>
                     <>
-                        <Header />
+                        <Header currentUser={currentUser} />
                         <Switch>
                             <Route exact path="/" component={App} />
                             <Route path="/profile/:id" component={Profile} />
@@ -29,6 +30,16 @@ const Root = () => (
         }}
     </Query>
 );
+
+const ME_QUERY = gql`
+{
+    me {
+        id
+        email
+        username
+    }
+}
+`
 
 const GET_TRACKS_QUERY = gql`
 {
